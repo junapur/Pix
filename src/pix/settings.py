21252aa5363e2aba155792a40cd2c8c6c7c41987
june <176018493+junapur@users.noreply.github.com>
2026-02-16 15:17:28 -0500

    feat: add settings parser

--- src/pix/settings.py
+++ src/pix/settings.py
@@ -1,10 +1,13 @@
 from typing import Literal
 
-from pydantic import SecretStr
+from pydantic import SecretStr, ValidationError
 from pydantic_settings import (
     BaseSettings,
+    PydanticBaseSettingsSource,
     SettingsConfigDict,
 )
+from rich.console import Console
+from rich.tree import Tree
 
 
 class Settings(BaseSettings):
@@ -17,3 +20,40 @@ class Settings(BaseSettings):
         env_file_encoding="utf-8",
         env_prefix="PIX_",
     )
+
+    @classmethod
+    def settings_customise_sources(
+        cls,
+        settings_cls: type[BaseSettings],
+        init_settings: PydanticBaseSettingsSource,
+        env_settings: PydanticBaseSettingsSource,
+        dotenv_settings: PydanticBaseSettingsSource,
+        file_secret_settings: PydanticBaseSettingsSource,
+    ) -> tuple[PydanticBaseSettingsSource, ...]:
+        return init_settings, env_settings, dotenv_settings
+
+
+def load_settings() -> Settings:
+    try:
+        return Settings()  # pyright: ignore[reportCallIssue] https://github.com/pydantic/pydantic-settings/issues/201
+    except ValidationError as e:
+        _display_errors(e)
+        raise SystemExit(1) from e
+
+
+def _display_errors(error: ValidationError) -> None:
+    console = Console(stderr=True)
+    count = error.error_count()
+    plural = "s" if count > 1 else ""
+
+    tree = Tree(f"[red]Failed to validate settings ({count} error{plural})[/]")
+
+    for err in error.errors():
+        msg = err["msg"]
+        value = repr(err["input"])
+        field = ".".join(str(loc) for loc in err["loc"])
+
+        tree.add(f"[bold]{field}[/]: {msg} - got [italic red]{value}[/]")
+
+    console.print(tree)
+    console.print("\n[dim]Tip: Check your .env file or environment variables.[/]")
