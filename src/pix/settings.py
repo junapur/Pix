f780f885420478e8830f9493120990aa07770bd7
june <176018493+junapur@users.noreply.github.com>
2026-02-19 22:11:01 -0500

    refactor(settings): clean up code

--- src/pix/settings.py
+++ src/pix/settings.py
@@ -1,6 +1,9 @@
 from typing import Literal
 
-from pydantic import SecretStr, ValidationError
+from pydantic import (
+    SecretStr,
+    ValidationError,
+)
 from pydantic_settings import (
     BaseSettings,
     PydanticBaseSettingsSource,
@@ -12,7 +15,6 @@ from rich.console import Console
 class Settings(BaseSettings):
     token: SecretStr
     log_level: Literal["DEBUG", "INFO", "WARN", "ERROR", "CRITICAL"]
-
     model_config = SettingsConfigDict(
         frozen=True,
         env_file=".env",
@@ -35,22 +37,22 @@ class Settings(BaseSettings):
 def load_settings() -> Settings:
     try:
         return Settings()  # pyright: ignore[reportCallIssue] https://github.com/pydantic/pydantic-settings/issues/201
-    except ValidationError as e:
-        _display_errors(e)
-        raise SystemExit(1) from e
+    except ValidationError as exc:
+        _display_errors(exc)
+        raise SystemExit(1) from exc
 
 
 def _format_loc(loc: tuple[str | int, ...]) -> str:
     path = ""
 
-    for idx, segment in enumerate(loc):
-        match segment:
+    for idx, part in enumerate(loc):
+        match part:
+            case str() if idx > 0:
+                path += f".{part}"
             case str():
-                if idx > 0:
-                    path += "."
-                path += segment
+                path += part
             case int():
-                path += f"[{segment}]"
+                path += f"[{part}]"
 
     return path
 
